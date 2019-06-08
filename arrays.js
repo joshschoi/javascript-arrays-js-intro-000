ba7ada3679da1b8c9873bb6fe2ccfe04377a5c7a
Joshua Choi <jsc.r839@gmail.com>
2019-06-08 19:52:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  var newarray = array;
-  return newarray.unshift(element);
+  array.unshift(element);
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
